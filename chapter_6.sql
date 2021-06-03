67b1878132bb9afe140f717ec5f9f90638fa8c1d
kjmyeong <cptm2399@gmail.com>
2021-06-03 09:47:20 +0900

    0603 sql

--- chapter_6.sql
+++ chapter_6.sql
@@ -1,59 +1,153 @@
-SELECT
-    a.employee_id,
-    a.emp_name,
-    b.job_id,
-    b.department_id
-FROM
-    employees  a,
-    job_history  b
-WHERE
-        a.employee_id = b.employee_id (+)
-    AND a.department_id = b.department_id(+);
-
-SELECT
-    a.employee_id,
-    a.emp_name,
-    a.department_id,
-    b.department_name
-FROM
-    employees a,
-    departments b;
-    
---ANSI 조인  184p
-SELECT
-    a.employee_id,
-    a.emp_name,
-    b.department_id,
-    b.department_name
-FROM
-    employees    a,
-    departments  b
-WHERE
-        a.department_id = b.department_id
-    AND a.hire_date >= TO_DATE('2003-01-01', 'yyyy-mm-dd');
+-- 동등조인     176p
+SELECT a.employee_id, a.emp_name, a.department_id, b.department_name
+  FROM employees a,
+       departments b
+ WHERE a.department_id = b.department_id;
 
 
-SELECT
-    a.employee_id,
-    a.emp_name,
-    b.department_id,
-    b.department_name
-FROM
-    employees a
-INNER JOIN departments b ON ( a.department_id = b.department_id )
-WHERE a.hire_date >= TO_DATE('2003-01-01', 'yyyy-mm-dd');
+-- 세미조인     177p
+/*EXISTS*/
+SELECT department_id, department_name
+  FROM departments a
+ WHERE EXISTS ( SELECT * 
+                 FROM employees b
+                WHERE a.department_id = b.department_id
+                  AND b.salary > 3000)
+ORDER BY a.department_name;
 
-select a.employee_id, a.emp_name, b.department_id, b.department_name
-from employees a,
-     departments b
-where a.department_id = b.department_id
-and a.hire_date >= to_date('2013-01-01', 'yyyy-mm-dd');
+/*IN*/
+SELECT department_id, department_name
+  FROM departments a
+ WHERE a.department_id  IN ( SELECT b.department_id
+                               FROM employees b
+                              WHERE b.salary > 3000)
+ORDER BY department_name;
+
+/*일반조인 => 결과수 많아짐*/
+SELECT a.department_id, a.department_name
+  FROM departments a, employees b
+ WHERE a.department_id = b.department_id
+   AND b.salary > 3000
+ORDER BY a.department_name;
 
 
+-- 안티조인     179p
+SELECT a.employee_id, a.emp_name, a.department_id, b.department_name
+  FROM employees a,
+       departments b
+ WHERE a.department_id = b.department_id
+   AND a.department_id NOT IN ( SELECT department_id
+                                  FROM departments 
+                                 WHERE manager_id IS NULL) ;
 
 
+SELECT count(*)
+  FROM employees a
+ WHERE NOT EXISTS ( SELECT 1
+                      FROM departments c
+                     WHERE a.department_id = c.department_id 
+                       AND manager_id IS NULL) ;
 
 
+-- 셀프조인     180p
+SELECT a.employee_id, a.emp_name, b.employee_id, b.emp_name, a.department_id
+  FROM employees a,
+        employees b
+ WHERE a.employee_id < b.employee_id      
+   AND a.department_id = b.department_id
+   AND a.department_id = 20;
+   
+-- 외부조인     181p
+/*일반조인*/
+SELECT a.department_id, a.department_name, b.job_id, b.department_id
+FROM departments a,
+     job_history b
+WHERE a.department_id = b.department_id;
+
+/*외부조인*/
+SELECT a.department_id, a.department_name, b.job_id, b.department_id
+FROM departments a,
+     job_history b
+WHERE a.department_id = b.department_id (+) ;
 
 
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id 
+  FROM employees a,
+       job_history b
+ WHERE a.employee_id = b.employee_id(+)
+   AND a.department_id = b.department_id;
 
+
+select a.employee_id, a.emp_name, b.job_id, b.department_id 
+  from employees a,
+       job_history b
+ where a.employee_id  = b.employee_id(+)
+   and a.department_id = b.department_id(+);
+
+
+-- 카타시안 조인      184p
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+  FROM employees a,
+       departments b;
+
+
+-- ANSI 조인      184p
+/*기존문법*/
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+  FROM employees a,
+       departments b
+ WHERE a.department_id = b.department_id
+   AND a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+/*ANSI문법*/
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+  FROM employees a
+  INNER JOIN departments b
+    ON (a.department_id = b.department_id )
+ WHERE a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+/*잘못된 경우*/
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+  FROM employees a
+  INNER JOIN departments b
+    USING (department_id)
+ WHERE a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+/*잘된 경우*/
+SELECT a.employee_id, a.emp_name, department_id, b.department_name
+  FROM employees a
+  INNER JOIN departments b
+    USING (department_id)
+ WHERE a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+
+-- ANSI 외부조인        186p
+select a.employee_id, a.emp_name, b.job_id, b.department_id 
+  from employees a,
+       job_history b
+ where a.employee_id  = b.employee_id(+)
+   and a.department_id = b.department_id(+);
+
+/*LEFT*/
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id 
+  FROM employees a
+  LEFT OUTER JOIN job_history b
+  ON ( a.employee_id  = b.employee_id
+       and a.department_id = b.department_id) ;
+
+/*RIGHT*/
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id 
+  FROM job_history b 
+  RIGHT OUTER JOIN employees a
+  ON ( a.employee_id  = b.employee_id
+       and a.department_id = b.department_id) ;
+
+/*OUTER 생략*/
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id 
+  FROM employees a
+  LEFT JOIN job_history b
+  ON ( a.employee_id  = b.employee_id
+       and a.department_id = b.department_id) ;
+       
+       
+       
